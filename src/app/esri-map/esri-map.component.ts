1e69742fe016f69c11efc6384cf79ddd61388964
Jonathan Cao <jondcao@gmail.com>
2018-11-28 00:10:35 -0800

    Update esri-map.component.ts

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -1,20 +1,9 @@
-/*
-  Copyright 2018 Esri
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
 import { Component, OnInit, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
 import { loadModules } from 'esri-loader';
 import esri = __esri;
 import { viewAttached } from '@angular/core/src/render3/instructions';
+import { NewProjectService } from '../new-project.service';
+
 
 
 @Component({
@@ -29,6 +18,8 @@ export class EsriMapComponent implements OnInit {
   private _zoom = 11.4;
   private _center = [-118.2437, 34.0522];
   private _basemap = 'streets';
+
+  
 
   @Input()
   set zoom(zoom: number) {
@@ -62,16 +53,20 @@ export class EsriMapComponent implements OnInit {
   // this is needed to be able to create the MapView at the DOM element in this component
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
 
-  constructor() { }
+  constructor(private _newProjectService: NewProjectService) { }
+
+ 
+ 
 
   public ngOnInit() {
-
+    
     // required modules to import
     loadModules([
       'esri/Map',
       'esri/views/MapView',
       'esri/layers/Layer',
       'esri/layers/FeatureLayer',
+      
       
       'dojo/domReady!'
       
@@ -126,26 +121,29 @@ export class EsriMapComponent implements OnInit {
         }
 
         function getGraphics(response){
-          console.log(response.results)
-
-          if(response.results.length)
-          {
-            
-             var graphic = response.results.filter(function(result){
-               return result.graphic.layer === centerLineData;
-             })[0].graphic;
-
-             //console.log(graphic)
-             
-            
-          }
-
-          console.log("HELLO")
-          
-        }
-        
       
+        let street = {assetId:"", name:""}
+       
+      
+        if(response.results.length == 2)
+        {
+          
+          
 
+          street.assetId = response.results[0].graphic.attributes.ASSETID;
+          street.name = response.results[0].graphic.attributes.STNAME + " " + response.results[0].graphic.attributes.STSFX
+
+          //this._newProjectService.setStreet(street);
+
+          console.log("______________________________")
+          console.log("ASSET ID: " + street.assetId)
+          console.log("STREET NAME: " + street.name)
+         
+
+        }
+
+      }
+        
 
       })
       .catch(err => {
