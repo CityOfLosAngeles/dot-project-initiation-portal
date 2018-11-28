a6c9ac3af6c53d1779a84df180c17d3ff5661225
Jonathan Cao <jondcao@gmail.com>
2018-11-28 00:14:13 -0800

    Update street-select.component.ts

--- src/app/street-select/street-select.component.ts
+++ src/app/street-select/street-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { NewProjectService } from '../new-project.service';
 
 @Component({
   selector: 'app-street-select',
@@ -7,11 +8,30 @@ import { Component, OnInit, Input} from '@angular/core';
 })
 export class StreetSelectComponent implements OnInit {
 
- 
+  public project = { title: '', leadName: '', leadEmail: '', leadPhone:'', leadTitle:'', leadDivision:'', leadSupervisior:'',
+          category: '', modal:'', prelimStart: '', prelimEnd: '', engageStart: '', engageEnd: '', developStart: '',
+          developEnd: '', designStart: '', designEnd: '', bidAwardStart: '', bidAwardEnd: '', implementStart: '',
+          implementEnd: '', level:"", background:"", statement:"", goals:"", description:"", primaryPS:"", cs1:"",
+          cs2:"", boundaries:"", councilDistricts:"", stakeholders:"", agencies:"", lifeCyclePhase:"", active:"",
+          funded:"", costEstimate:"", budget:"", fundingSource:"", potentialSource:"", attemptedSource:"",
+          futureSource:"", constructionMethods:"", milestones:"",
+}
 
-  constructor() { }
+  constructor(private _newProjectService: NewProjectService) { }
+
+  
+
+
 
   ngOnInit() {
+    // on inititation of this component, grab the data from the service class and assign it to this component so that it can be used
+    this.project = this._newProjectService.getData();
+   
+   
+  }
+
+  onClick(){
+   
   }
 
   
